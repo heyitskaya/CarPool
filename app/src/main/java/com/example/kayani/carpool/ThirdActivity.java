a8ebc7d2ab3cb2a2c1224a9dd6368e8fd56fa23c
heyitskaya <ni23x@mtholyoke.edu>
2016-04-11 01:01:08 -0400

    First and Second Activity working

---
+++ app/src/main/java/com/example/kayani/carpool/ThirdActivity.java
@@ -0,0 +1,43 @@
+package com.example.kayani.carpool;
+//import android.app.DialogFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+//right now this is only for people going to bradley
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent secondIntent=getIntent();
+       // DialogFragment newFragment = new DatePickerFragment();
+        //newFragment.show(getSupportFragmentManager(), "datePicker");
+
+    }
+
+    public void showDatePickerDialog(View v) {
+
+    }
+
+}
