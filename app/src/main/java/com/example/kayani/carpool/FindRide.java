90c77cee79745c612af53463385042bfc8080337
heyitskaya <ni23x@mtholyoke.edu>
2016-04-18 22:08:59 -0400

    All activities working, not yet changed MainActivity to login

--- app/src/main/java/com/example/kayani/carpool/FindRide.java
+++ app/src/main/java/com/example/kayani/carpool/FindRide.java
@@ -1,16 +1,22 @@
 package com.example.kayani.carpool;
+
 /**
  * Created by kayani on 4/12/16.
  */
 
+//this is all the same location right now
 public class FindRide {
     private QueueLL<Passenger>[][] passengerMatrix; // 7 rows 24 columns
+    private QueueLL<Driver>[][] driverMatrix= new QueueLL[7][24];
+
 
     public FindRide(Passenger p){
+
         passengerMatrix= new QueueLL[7][24];
         for(int i=0;i<7;i++){
             for(int j=0;j<24;j++){
                 passengerMatrix[i][j]= new QueueLL<Passenger>(); //initialize the 2d array
+                driverMatrix[i][j]= new QueueLL<Driver>();
             }
         }
         int day=p.getDay();
@@ -18,10 +24,19 @@ public class FindRide {
         //row by column
         //row: hour
         //column: day
+      /**  driverMatrix[day-18][hour-7].enqueue(new Driver(p.getLocation(),p.getDay(),p.getHour()));
+        passengerMatrix[day-18][hour-7].enqueue(p); //add this to the queue
 
-        passengerMatrix[day-1][hour-4].enqueue(p); //add this to the queue
+        if(driverMatrix[day-18][hour-7]!=null && passengerMatrix[day-18][hour-7]!=null){
+            Log.d("Have we found a ride?","yes");
 
 
+        } **/
+
+    }
+
+    public boolean existsRide(){
+        return true;
     }
 
 
--- app/src/main/java/com/example/kayani/carpool/RideNotFound.java
+++ app/src/main/java/com/example/kayani/carpool/RideNotFound.java
@@ -1,5 +1,4 @@
 package com.example.kayani.carpool;
-import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,15 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TimePicker;
-@TargetApi(23)
-/** for picking out a time**/
-public class FourthActivity extends AppCompatActivity {
+
+public class RideNotFound extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fourth);
+        setContentView(R.layout.activity_ride_not_found);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,26 +25,7 @@ public class FourthActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         Intent intent=getIntent();
-    }
-
-    public void goToFifthActivity(View view){
-        Passenger currentPassenger= new Passenger(PassengerData.getLocation(),PassengerData.getDay(), PassengerData.getTime());
-
-        TimePicker timePicker= (TimePicker) findViewById(R.id.timePicker);
-        PassengerData.setTime(timePicker.getHour());
-        FindRide findRide= new FindRide(currentPassenger);
-        if(findRide.existsRide()==true){
-            Intent intent= new Intent(this,FifthActivity.class);
-            startActivity(intent);
-        }
-       /** else{
-            Intent intent= new Intent(this,RideNotFound.class);
-            startActivity(intent);
-
-        } **/
-
     }
 
 }
